6db684028228a56af1626d0e518b657eea01003c
kylemhicks <kyle.hicks@nps.edu>
2026-04-26 20:26:17 -0700

    cesium serverwide

--- app-config.js
+++ app-config.js
@@ -1,4 +1,4 @@
 window.EW_SIM_CONFIG = {
   apiBaseUrl: window.location.origin + "/api",
-  cesiumIonDefaultToken: ""
+  cesiumIonDefaultToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIxYzE0M2I3Ni1jYTNiLTRiZmQtYjA3Yi1iNDBlYTU5N2IyZjgiLCJpZCI6MzMxODMxLCJpYXQiOjE3NzcyNjAzNjN9.WmyUT7DQIV-T1Riu7itdlY6spyIPd1FqnUErfly4IX8"
 };
